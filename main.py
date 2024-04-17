5bd196392d836cdb0e0768af60cca74843d091d6
yokopipi <yokozuka.yoshiaki@gmail.com>
2024-04-18 00:02:00 +0900

    mainの再作成

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # サブモジュールをインポートする
-# import
+from create_iclist_ import create_iclist
+from golfcourse_search_ import golfcourse_search
+from route_search_ import route_search
+from highway_toll_ import highway_toll
+from cost_calculation_ import cost_calculation
+from route_display_ import route_display
 
 import streamlit as st
 import pandas as pd
 import numpy as np
+import datetime
+
+# ページの設定：広いレイアウトを使用
+st.set_page_config(layout="wide")
 
 set_prefecture_list = {
     "東京都":1,
@@ -23,58 +32,40 @@ set_starttime_list = {
 
 
 
+# サイドバー設定
+with st.sidebar:
+    st.markdown("**【ゴルフ場検索条件】:**")
+    area = st.selectbox("エリア", set_prefecture_list.keys())
+    # 今日の日付をデフォルト値として設定
+    today = datetime.date.today()  # 今日の日付を取得
+    play_date = st.date_input("プレー日", today)  # デフォルト値として今日の日付を使用
+    min_fee = st.number_input("最低金額", value=8000)
+    max_fee = st.number_input("最高金額", value=12000)
+    start_times = st.selectbox("スタート時間", set_starttime_list.keys())
+    if st.button("ゴルフ場検索"):
+        # 検索実行
+        golfcourse_df = golfcourse_search(area, play_date, min_fee, max_fee, start_times)
+        # セッション状態に保存
+        st.session_state.golfcourse_df = golfcourse_df
 
-tab1, tab2 = st.tabs(["main", "member"])
+# メイン表示
+if 'golfcourse_df' in st.session_state:
+    st.markdown("**ゴルフコース一覧:**")
 
-with tab1:
-    st.title("ワイヤーフレーム")
-    st.sidebar.write("＜対象ゴルフ場＞")
-    st.sidebar.selectbox("月", np.arange(1,13))
-    st.sidebar.selectbox("日", np.arange(1,32))
-    st.sidebar.selectbox("日間", np.arange(1,32))
+    for i, row in st.session_state.golfcourse_df.iterrows():
+        # Markdownを使ってゴルフコース名をハイパーリンクとして表示
+        link = f"[{row['golf_course_name']}]({row['url']})"
+        cols = st.columns([2, 0.5, 0.5, 0.5, 2, 1, 0.5])
+        cols[0].markdown(link, unsafe_allow_html=True)
+        cols[1].write(row['min_price'])
+        cols[2].write(row['max_price'])
+        cols[3].write(row['rating'])
+        cols[4].write(row['address'])
+        cols[5].write(row['distance_from_highway'])
+        
+        # ボタンの配置
+        if cols[6].button("選択", key=f"select_{i}"):
+            st.session_state.selected_address = row['address']
 
-    st.sidebar.selectbox("エリア", set_prefecture_list.keys())
-    st.sidebar.selectbox("スタート時間", set_starttime_list.keys())
-
-    st.sidebar.button("検索")
-
-    st.sidebar.text_input("ゴルフ場名検索")
-
-    st.sidebar.button("検索２")
-
-    st.sidebar.write("＜ピックアップ情報＞")
-    st.sidebar.selectbox("出発地", set_prefecture_list.keys())
-    st.sidebar.selectbox("1人目", set_prefecture_list.keys())
-    st.sidebar.selectbox("2人目", set_prefecture_list.keys())
-    st.sidebar.selectbox("3人目", set_prefecture_list.keys())
-
-    st.sidebar.write("＜交通費情報＞")
-    st.sidebar.text_input("燃費[L/km]")
-    st.sidebar.text_input("ガソリン代[円]")
-
-    df_Gcourse = pd.DataFrame(np.arange(12).reshape(3, 4))
-
-    st.table(df_Gcourse)
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.write("＜移動情報＞")
-        st.write("出発")
-
-        st.write("1人目")
-        st.write("2人目")
-        st.write("3人目")
-        st.write("目的")
-
-        st.write("総距離")
-        st.write("総時間")
-
-    with col2:
-        st.write("＜交通費＞")
-        st.write("高速料金")
-        st.write("ガソリン代")
-        st.write("1人あたり交通費")
-
-with tab2:
-    st.write("メンバー登録？")
+    if 'selected_address' in st.session_state:
+        st.write(f"Selected Address: {st.session_state.selected_address}")
